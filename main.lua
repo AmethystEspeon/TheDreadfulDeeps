9455bbdab48456a39675f69c2aeb8bb37fdfdd7b
AmethystEspeon <dire.gizmos@gmail.com>
2022-09-22 14:35:34 -0700

    Deleted the vscode stuff cause it's not needed anymore (actually used local correctly now :D)
    -Changed some of the code to be more reusable later on and easier to change, namely healthbar related things
    -Added more variables at the top so that it's both easier to read and easier to change
    -Added working HP bars for enemies.
    
    NOTE: None of the hp bars actually do anything yet besides show %hp. Doesn't check for negatives.

--- main.lua
+++ main.lua
@@ -2,53 +2,70 @@ local function debugPrint(...)
     print(...)
 end
 
---TODO: Make healthbar change based on health%
-
---HEALTHBAR SIZES
+--BAR SIZES--
 local WIDTH = 150
-local HEIGHT = 75
+local HEIGHT = 60
 
-scene = "fight"
-local enemies = {}
+--POSITIONS--
+local CENTERX = (love.graphics.getWidth()-WIDTH)*0.5
+local CENTERY = (love.graphics.getHeight()-HEIGHT)*0.5
+local STARTINGENEMYBARX = CENTERX+1.8*WIDTH
+local STARTINGENEMYBARY = love.graphics.getHeight()-(9)*(HEIGHT+3)
+
+--USED VARIABLES--
+local scene = "fight"
+local enemies = {{baseHealth = 300, baseAtk = 10, maxHealth = 300, currentHealth = 300},{baseHealth = 300, baseAtk = 10, maxHealth = 300, currentHealth = 300}}
 local allies = {}
-local player = {baseHealth = 300, baseAtk = 10, maxHealth = 300, currentHealth = 300, currentAtk = 10}
+local player = {baseHealth = 300, baseAtk = 10, baseMana = 100, maxHealth = 300, currentHealth = 300, maxMana= 100, currentMana = 100}
 local playerBars = {health = {}, mana = {}}
 
 function love.load()
-    createPlayerBars()
+    --createPlayerBars()
 end
 
-function createPlayerBars()
+
+local function drawPlayerBars()
+    ----Background----
     love.graphics.setColor(255,255,255)
-    playerBars.health.background = love.graphics.rectangle("fill", love.graphics.getWidth()*0.43,love.graphics.getHeight()*(2/3), WIDTH+1, HEIGHT+1)
-    playerBars.mana.background = love.graphics.rectangle("fill", love.graphics.getWidth()*0.43,love.graphics.getHeight()*(2/3)+HEIGHT+2, WIDTH+1, HEIGHT/4+1)
+    --Health--
+    love.graphics.rectangle("fill", CENTERX-1,love.graphics.getHeight()*(2/3)-1, WIDTH+2, HEIGHT+2)
+    --Mana--
+    love.graphics.rectangle("fill", CENTERX-1,love.graphics.getHeight()*(2/3)+HEIGHT+1, WIDTH+2, HEIGHT/4+2)
+
+    ----Bars----
+    --Health Bar--
+    local green = 1 * (player.currentHealth/player.maxHealth)
+    local red = 1 - green
+    love.graphics.setColor(red, green, 0)
+    love.graphics.rectangle("fill", CENTERX, love.graphics.getHeight()*(2/3), WIDTH*player.currentHealth/player.maxHealth, HEIGHT)
+
+    --Mana Bar--
+    love.graphics.setColor(0,0,225)
+    love.graphics.rectangle("fill", CENTERX,love.graphics.getHeight()*(2/3)+HEIGHT+2, WIDTH*player.currentMana/player.maxMana, HEIGHT/4)
 end
+
+local function drawEnemyBar(i)
+    local enemy = enemies[i]
+    ----Background----
+    love.graphics.setColor(255,255,255)
+    love.graphics.rectangle("fill", STARTINGENEMYBARX-1,STARTINGENEMYBARY+i*(HEIGHT/2+6)-1, WIDTH+2, HEIGHT/2+2)
+
+    ----Bar----
+    love.graphics.setColor(255, 0, 0)
+    love.graphics.rectangle("fill", STARTINGENEMYBARX, STARTINGENEMYBARY+i*(HEIGHT/2+6), WIDTH*enemy.currentHealth/enemy.maxHealth, HEIGHT/2)
+end
+
 
 function love.draw()
     if scene == "fight" then
-        --love.graphics.draw(playerBars.health.background)
-        --love.graphics.draw(playerBars.mana.background)
-        drawPlayerBars()--[[
+        drawPlayerBars()
         for i = 1, #enemies do
-            drawEnemyBars(i)
+            drawEnemyBar(i)
         end
 
-        for i = 1, #allies do
+        --[[for i = 1, #allies do
             drawAllyBars(i)
         end]]
     end
 end
 
-function drawPlayerBars()
-    local green = 1 * (player.currentHealth/player.maxHealth)
-    local red = 1 - green
-    love.graphics.setColor(red, green, 0)
-    playerBars.health.healthBar = love.graphics.rectangle("fill", love.graphics.getWidth()*0.43,love.graphics.getHeight()*(2/3), WIDTH*player.currentHealth/player.maxHealth, HEIGHT)
-    if player.currentHealth > 0 then
-        player.currentHealth = player.currentHealth - 1
-    elseif player.currentHealth <= 0 then
-        player.currentHealth = player.maxHealth
-    end
-
-    debugPrint("Green: " .. green .. "\nRed: " .. red)
-end
